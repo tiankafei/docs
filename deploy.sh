ff5767df60bb1fb7a0d0d3770ae5d287e1806696
weishuangshuang <weishuangshuang@thtf.com.cn>
2020-06-15 17:37:46 +0800

    提交

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+java -jar node-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd dist
+
+# 如果是发布到自定义域名
+echo 'tiankafei.top' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:tiankafei/tiankafei.github.io.git master
+
+cd -
