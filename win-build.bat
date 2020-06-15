36c3eb1b6af66c1c29cf24e8515d37a355cf1446
weishuangshuang <weishuangshuang@thtf.com.cn>
2020-06-15 11:28:52 +0800

    提交

--- win-build.bat
+++ win-build.bat
@@ -1,7 +1,8 @@
 cd /d %~dp0
 echo %~dp0
 
-call rd/s/q %~dp0.vuepress\public
-
+call rd /q/s %~dp0dist
+call java -jar node-1.0-SNAPSHOT-jar-with-dependencies.jar
+call yarn docs:build
 
 pause
--- win-dev.bat
+++ win-dev.bat
@@ -2,5 +2,6 @@ cd /d %~dp0
 echo %~dp0
 
 call java -jar node-1.0-SNAPSHOT-jar-with-dependencies.jar
+call yarn docs:dev
 
 pause
